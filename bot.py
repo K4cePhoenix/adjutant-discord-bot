bd46457c53612c40a258ead6c91452c85e0c2e2c
K4cePhoenix <phoenixmkiv@googlemail.com>
2018-04-08 22:36:01 +0200

    added files

---
+++ bot.py
@@ -0,0 +1,96 @@
+from datetime import datetime, timedelta
+from discord.ext import commands
+import asyncio
+import discord
+import logging
+import os
+import platform
+import pytz
+import toml
+
+
+""" SC2 Event Bot
+posts informations of open sc2 tournaments.
+"""
+
+def get_prefix(bot, message):
+    """ A callable Prefix for our bot.
+    This could be edited to allow per server prefixes.
+    In direct messages, only one prefix is allowed, while in a server,
+    the user is allowed to mention us or use any of the prefixes in our list.
+    """
+    prefixes = ['a>', 'Adjutant ', 'adjutant ', 'a!']
+    if (not message.guild):
+        return '!'
+    return commands.when_mentioned_or(*prefixes)(bot, message)
+
+initial_extensions = ['ext.sc2openevents']# ,
+                     # 'ext.lickypiddy']
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='bot.log', encoding='utf-8', mode='w')
+logger.addHandler(handler)
+
+bot = commands.Bot(description=('Adjutant 10-32 Discord Bot by Phoenix#2694'), command_prefix=get_prefix)
+
+@bot.event
+async def on_ready():
+    guilds = len(bot.guilds)
+    channels = len([key for key in bot.get_all_channels()])
+    users = len(set(bot.get_all_members()))
+    print('Logged in as {} (ID: {})'.format(bot.user.name, bot.user.id))
+    print('\nConnected to:')
+    print('{} guilds | {} channels | {} users\n'.format(guilds, channels, users))
+    print('-------------')
+    print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
+    print('\nUse this link to invite {}:'.format(bot.user.name))
+    print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot'.format(bot.user.id))
+    print('-------------')
+    print('You are running SC2 Events Bot v'+conf['owner']['version']+' by Phoenix#2694')
+    print('Ready at  {:%b %d, %H:%M (%Z)}'.format(datetime.now(tz=pytz.utc)))
+    await bot.change_presence(activity=discord.Game(name='with bugs..'))
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+            logger.info('Loaded extension {}'.format(extension))
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            logger.error('Failed to load extension {} - {}'.format(extension, exc))
+
+
+@bot.event
+async def on_guild_join(guild):
+    if guild.me.guild_permissions.change_nickname:
+        await guild.me.edit(nick='Adjutant 10-32')
+    else:
+        print("Couldn't change my name on {}".format(guild.name))
+
+
+@bot.event
+async def on_command(ctx):
+    message = ctx.message
+    destination = None
+    if isinstance(ctx.channel, discord.abc.GuildChannel):
+        destination = 'Private Message'
+    else:
+        destination = '#{0.channel.name} ({0.guild.name})'.format(message)
+    logger.info('{0.created_at}: {0.author.name} in {1}: {0.content}'.format(message, destination))
+
+
+if __name__ == '__main__':
+
+
+    conf_path = './data/bot/'
+    conf_name = 'conf.toml'
+    if os.path.isdir(conf_path) is False:
+        logger.critical('Could not find folder {}'.format(conf_path))
+    elif os.path.isfile(conf_path+conf_name) is False:
+        logger.critical('Could not find config file in {}'.format(conf_path))
+    else:
+        conf = toml.load(conf_path+conf_name)
+        bot.run(conf['owner']['token'])
+    handlers = logger.handlers[:]
+    for hdlr in handlers:
+        hdlr.close()
+        logger.removeHandler(hdlr)
---
+++ ext/sc2/kuevst.py
@@ -0,0 +1,159 @@
+#import requests
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from datetime import datetime, timezone
+import pytz
+
+
+def get_name(data):
+    """ Returns the name of the event """
+    n = data.a.string
+    return n
+
+
+def get_cd(data):
+    """ Returns the countdown of the event
+    e.g. event sign up ends on 10:00 on November 4th
+         currently is 03:00, November 3rd
+         returns: datetime object with the following info
+         +1day7hours
+    """
+    cdTime = datetime.strptime(
+        data.text[1:-5] + ' +0900', '%B %d, %Y - %H:%M %z')
+    currentTime = datetime.now(tz=pytz.timezone('Asia/Seoul'))
+    return cdTime - currentTime
+
+
+def get_time(data):
+    """ Returns the start date of the event """
+    eventTime = datetime.strptime(data.text[1:-5], '%B %d, %Y - %H:%M')
+    allTimes = time_to_times(eventTime)
+    eventTimeStr = times_to_string(allTimes)
+    return eventTimeStr
+
+
+def time_to_times(data):
+    """ Returns the event date for 3 timeszones """
+    timeKR = pytz.timezone('Asia/Seoul').localize(data)
+    timeAM = timeKR.astimezone(pytz.timezone('PST8PDT'))
+    timeEU = timeKR.astimezone(pytz.timezone('Europe/Berlin'))
+    return timeKR, timeEU, timeAM
+
+
+def times_to_string(data):
+    """ Returns beautiful date data """
+    timeStr = data[0].strftime("%B") + ' ' + data[0].strftime("%#d") + ', ' + data[0].strftime(
+        "%#I") + data[0].strftime("%p") + ' ' + data[0].strftime("%Z") + ' ( '
+    if data[1].strftime("%d") == data[0].strftime("%d"):
+        timeStr += data[1].strftime("%#I") + data[1].strftime("%p") + \
+            ' ' + data[1].strftime("%Z") + ' / '
+    else:
+        timeStr += data[1].strftime("%b") + ' ' + data[1].strftime("%#d") + ', ' + data[1].strftime(
+            "%#I") + data[1].strftime("%p") + ' ' + data[1].strftime("%Z") + ' / '
+    if data[2].strftime("%d") == data[0].strftime("%d"):
+        timeStr += data[2].strftime("%#I") + data[2].strftime("%p") + \
+            ' ' + data[2].strftime("%Z") + ' )'
+    else:
+        timeStr += data[2].strftime("%b") + ' ' + data[2].strftime("%#d") + ', ' + data[2].strftime(
+            "%#I") + data[2].strftime("%p") + ' ' + data[2].strftime("%Z") + ' )'
+    return timeStr
+
+
+def get_mode(data):
+    """ Nothing to see here, literally """
+    return None
+
+
+def get_rsl(data, sL):
+    """ Returns Region, Server and/or League information """
+    r = data('td')[5].text.split('(')[0][1:-1]
+    if len(data('td')[5].text.split('(')) == 2:
+        s = data('td')[5].text.split('(')[1][0:-2]
+    else:
+        s = None
+    if len(data('td')[4].text.split('(')) == 2:
+        l = data('td')[4].text.split('(')[0][1:]
+    else:
+        l = data('td')[4].text.split('(')[0][1:-1]
+    if l == 'Online Qualifier':
+        l = None
+    if l == 'Online':
+        l = None
+    if l == 'Offline':
+        l = None
+    return r, s, l
+
+
+def get_prize(data):
+    """ Returns any prizepool, if any """
+    if data.string == None:
+        for el in range(0, len(data.text.split('+'))):
+            if el == 0:
+                pr = data.text.split('+')[el][1:]
+            elif el == len(data.text.split('+')) - 1:
+                pr += ' + ' + data.text.split('+')[el][1:-1]
+            else:
+                pr += ' + ' + data.text.split('+')[el][1:]
+    else:
+        pr = data.string[1:-1]
+    return pr
+
+
+def get_bracket(data):
+    """ Returns the bracket, which is in nearly all cases the signup page as well """
+    if data.find(class_="lp-icon lp-challonge") != None:
+        b = data.find(class_="lp-icon lp-challonge").parent.get('href')
+    elif data.find(class_="lp-icon lp-bracket") != None:
+        b = data.find(class_="lp-icon lp-bracket").parent.get('href')
+    else:
+        b = None
+    return b
+
+
+def get_matcherino(data):
+    """ Returns Matcherino information """
+    if data.find(class_="lp-icon lp-matcherino") != None:
+        ma = data.find(class_="lp-icon lp-matcherino").parent.get('href')
+    else:
+        ma = None
+    return ma
+
+def get_matcherino_code(data):
+    if data.find(class_="lp-icon lp-matcherino") != None:
+        code = data.find(class_="lp-icon lp-matcherino").parent.parent.get('title')
+    else:
+        code = None
+    return code
+
+
+def steal(tourType=None):
+    if tourType == 'general':
+        link = 'http://liquipedia.net/starcraft2/User:(16thSq)_Kuro/Open_Tournaments'
+    elif tourType == 'amateur':
+        link = 'http://liquipedia.net/starcraft2/User:(16thSq)_Kuro/Amateur_Tournaments'
+    elif tourType == 'team':
+        link = 'http://liquipedia.net/starcraft2/User:(16thSq)_Kuro/Team_Tournaments'
+    page = urlopen(link)
+    soup = BeautifulSoup(page, 'html.parser')
+    if tourType == 'general':
+        tableTour = soup.find_all('table')[1]
+    elif tourType == 'amateur':
+        tableTour = soup.find_all('table')[0]
+    elif tourType == 'team':
+        tableTour = soup.find_all('table')[1]
+    events = [['1'] * 8 for x in range(len(tableTour('tr')) - 2)]
+    for tRow in range(2, len(tableTour('tr'))):
+        countdown = get_cd(tableTour('tr')[tRow]('td')[0])
+        date = get_time(tableTour('tr')[tRow]('td')[1])
+        mode = get_mode(tableTour('tr')[tRow]('td')[2])
+        name = get_name(tableTour('tr')[tRow]('td')[3])
+        region, server, league = get_rsl(tableTour('tr')[tRow], tourType)
+        prize = get_prize(tableTour('tr')[tRow]('td')[6])
+        matcherino = get_matcherino(tableTour('tr')[tRow]('td')[7])
+        matcherinoCode = get_matcherino_code(tableTour('tr')[tRow]('td')[7])
+        bracket = get_bracket(tableTour('tr')[tRow]('td')[7])
+
+        events[tRow - 2] = [name, date, region, league, server, prize,
+                            matcherino, matcherinoCode, bracket, countdown, mode]
+    return events
