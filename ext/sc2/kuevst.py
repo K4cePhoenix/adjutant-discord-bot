d11baae87cc7c51cf0fc949f06f2954bab9a3440
K4cePhoenix <phoenixmkiv@googlemail.com>
2018-04-27 00:32:31 +0200

    dispel duplicates

--- ext/sc2/kuevst.py
+++ ext/sc2/kuevst.py
@@ -85,22 +85,10 @@ def get_rsl(data, sL):
 
 def get_prize(data):
     """ Returns any prizepool, if any """
-    # if data.string == None:
-    #     for el in range(0, len(data.text.split('+'))):
-    #         if el == 0:
-    #             pr = data.text.split('+')[el][1:]
-    #         elif el == len(data.text.split('+')) - 1:
-    #             pr += ' + ' + data.text.split('+')[el][1:-1]
-    #         else:
-    #             pr += ' + ' + data.text.split('+')[el][1:]
-    #     if pr[-1:] == '\n':
-    #         pr = pr[:-1]
-    # else:
-    #     pr = data.string[1:-1]
-
     for br in data.find_all("br"):
         br.replace_with("\n")
     pr = ' '.join(data.text.split('\n')).strip()
+    pr = ' '.join(pr.split('  '))
     if pr == "":
         pr = None
     elif pr == "-":
