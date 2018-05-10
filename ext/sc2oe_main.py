4ec51ada64cc7c0fc70b3e9cf44117ed7161c7c8
K4cePhoenix <phoenixmkiv@googlemail.com>
2018-05-10 15:30:58 +0200

    1 session for all 3 requests instead of 1 session per request

--- ext/sc2oe_main.py
+++ ext/sc2oe_main.py
@@ -151,25 +151,37 @@ class SC2OpenEvents():
         log.info(f'{pEvCount} / {aEvCount}  {evType} events already posted and {dEvCount} got deleted in {srv.name}')
 
 
-    async def fetch_soup(self, url, parser):
-        # Use a custom HTTP "User-Agent" header in your requests that identifies your project / use of the API, and includes contact information. 
-        headers = {'User-Agent': 'AdjutantDiscordBot (https://github.com/K4cePhoenix/Adjutant-DiscordBot; k4cephoenix@gmail.com)', 'Accept-Encoding': 'gzip'}
+    async def fetch_soups(self, url, eventTypes, parser):
+        soups = [[], [], []]
+        # Use a custom HTTP "User-Agent" header in your requests that identifies your project / use of the API, and includes contact information.
+        headers = {'User-Agent': 'Adjutant-DiscordBot (https://github.com/K4cePhoenix/Adjutant-DiscordBot; k4cephoenix@gmail.com)', 'Accept-Encoding': 'gzip'}
         async with aiohttp.ClientSession(headers=headers) as session:
-            async with session.get(url) as response:
-                json_body = await response.json()
-                return BeautifulSoup(json_body['parse']['text'], parser)
+            for ind, evt in enumerate(eventTypes):
+                params = {'action': 'parse', 'format': 'json', 'page': f'User:(16thSq)_Kuro/{evt}_Tournaments', 'prop': 'text', 'formatversion': '2'}
+                async with session.get(url, params=params) as response:
+                    json_body = await response.json()
+                    soups[ind] = BeautifulSoup(json_body['parse']['text'], parser)
+                    print(f'souped {evt}')
+                if ind != len(eventTypes)-1:
+                    print(f'{ind} sleeping...')
+                    # Liquipedia API usage guideline: "action=parse" [...] requests should not exceed 1 request per 30 seconds [...].
+                    await asyncio.sleep(30.1)
+            return soups
 
 
     async def check_all_events(self):
         events = [[], [], []]
         eventTypes = ['Open', 'Amateur', 'Team']
 
+        # for ind, evt in enumerate(eventTypes):
+        #     link = f'http://liquipedia.net/starcraft2/api.php?action=parse&format=json&page=User:(16thSq)_Kuro/{evt}_Tournaments&prop=text%7Cdisplaytitle&formatversion=2'
+        #     soup = await self.fetch_soup(link, eventTypes, 'html.parser')
+        #     if ind != len(eventTypes)-1:
+        #        await asyncio.sleep(30.1) 
+        #     events[ind] = kuevst.steal(evt, soup)
+        soups = await self.fetch_soups('http://liquipedia.net/starcraft2/api.php', eventTypes, 'html.parser')
         for ind, evt in enumerate(eventTypes):
-            link = f'http://liquipedia.net/starcraft2/api.php?action=parse&format=json&page=User:(16thSq)_Kuro/{evt}_Tournaments&prop=text%7Cdisplaytitle&formatversion=2'
-            soup = await self.fetch_soup(link, 'html.parser')
-            if ind == len(eventTypes)-1:
-               await asyncio.sleep(40) # Liquipedia API usage guideline: "action=parse" [...] requests should not exceed 1 request per 30 seconds [...].
-            events[ind] = kuevst.steal(evt, soup)
+            events[ind] = kuevst.steal(evt, soups[ind])
 
         log.info(f'Fetched {len(events[0])} general, {len(events[1])} amateur and {len(events[2])} team events')
         for guild in self.adjutant.guilds:
