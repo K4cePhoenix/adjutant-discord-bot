649a010d2e242f7ce237b1b7aaa1d4703c871989
K4cePhoenix <phoenixmkiv@googlemail.com>
2018-12-14 21:54:06 +0100

    clean up

--- ext/admin.py
+++ ext/admin.py
@@ -1,8 +1,5 @@
-from contextlib import redirect_stdout
 from discord.ext import commands
-import io
 import logging
-import textwrap
 import traceback
 
 from .utils import permissions as perms
@@ -16,67 +13,10 @@ class Administration:
         self.bot = bot
         self._lastResult = None
 
-    # def cleanup_code(self, content):
-    #     """Removes code blocks."""
-    #     if content.startswith('```') and content.endswith('```'):
-    #         return '\n'.join(content.split('\n')[1:-1])
-    #     return content.strip('` \n')
-
-    # @commands.command(name='eval')
-    # async def _eval(self, ctx, *, body: str):
-    #     """Evaluates a code"""
-    #     if perms._check(ctx, 5): # meh
-    #         env = {
-    #             'bot': self.bot,
-    #             'ctx': ctx,
-    #             'channel': ctx.channel,
-    #             'author': ctx.author,
-    #             'guild': ctx.guild,
-    #             'message': ctx.message,
-    #             '_': self._lastResult
-    #         }
-    #         env.update(globals())
-
-    #         body = self.cleanup_code(body)
-    #         stdout = io.StringIO()
-
-    #         to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
-
-    #         try:
-    #             exec(to_compile, env)
-    #         except Exception as e:
-    #             return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
-
-    #         func = env['func']
-    #         try:
-    #             with redirect_stdout(stdout):
-    #                 ret = await func()
-    #         except Exception as e:
-    #             value = stdout.getvalue()
-    #             await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
-    #         else:
-    #             value = stdout.getvalue()
-    #             try:
-    #                 await ctx.message.add_reaction('☑')
-    #             except:
-    #                 pass
-
-    #             if ret is None:
-    #                 if value:
-    #                     await ctx.send(f'```py\n{value}\n```')
-    #             else:
-    #                 self._lastResult = ret
-    #                 await ctx.send(f'```py\n{value}{ret}\n```')
-    #     else:
-    #         try:
-    #             await ctx.message.add_reaction('❌')
-    #         except:
-    #             pass
-
     @commands.command(name='load')
     async def _load(self, ctx, *, module):
         """Loads a module."""
-        if perms._check(ctx, 4): # Ducklings and meh
+        if perms._check(ctx, 4):
             try:
                 self.bot.load_extension('ext.'+module)
             except Exception as e:
@@ -95,7 +35,7 @@ class Administration:
     @commands.command(name='unload')
     async def _unload(self, ctx, *, module):
         """Unloads a module."""
-        if perms._check(ctx, 4): # Ducklings and meh
+        if perms._check(ctx, 4):
             try:
                 self.bot.unload_extension('ext.'+module)
             except Exception as e:
@@ -114,7 +54,7 @@ class Administration:
     @commands.command(name='reload')
     async def _reload(self, ctx, *, module):
         """Reloads a module."""
-        if perms._check(ctx, 4): # Ducklings and meh
+        if perms._check(ctx, 4):
             try:
                 self.bot.unload_extension('ext.'+module)
                 self.bot.load_extension('ext.'+module)
